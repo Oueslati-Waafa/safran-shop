855eb9dae94bec0a963934fc5b1fb3dca91748fd
ShiroNhL <shironhl2077@gmail.com>
2023-06-20 09:28:04 +0100

    'fixes-cloudinary'

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,17 +1,7 @@
 import multer from "multer";
 import { diskStorage } from "multer";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
 
 const storage = diskStorage({
-  destination: (req, file, callback) => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const directory = join(__dirname, "../public/images").replace(
-      /\\/g,
-      "/"
-    );
-    callback(null, directory);
-  },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
     const extension = file.mimetype.split("/")[1];
